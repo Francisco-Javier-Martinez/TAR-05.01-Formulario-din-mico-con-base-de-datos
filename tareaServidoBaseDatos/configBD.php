7bf9fb7d4a7fb4106523d4dbad4e02dcd6e167b1
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-10-23 17:49:31 +0200

    Update configBD.php

--- tareaServidoBaseDatos/configBD.php
+++ tareaServidoBaseDatos/configBD.php
@@ -1,6 +1,8 @@
 <?php
+    //he tenido que poner '127.0.0.1:3307' porque mi Xampp no iba lo he tenido que arreglar a mano y ahora debo poner esto que se lo puse a mano yo en mi archivo de configuracion
     define("SERVIDOR",'127.0.0.1:3307');
     define("USUARIO",'root');
     define("PASSWORD",'');
     define("BBDD",'cambioclimatico');
+
 ?>
